f021e80d43034ad6e0ada8eb1e3d95d558e48b19
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2024-06-02 22:05:01 +0200

    Update nixdev.sh

--- nixdev.sh
+++ nixdev.sh
@@ -3,7 +3,26 @@
 # Exit on error
 set -e
 
+# create log file
+touch ./setup.log
 
+# https://nixos.org/
+if command -v nix &> /dev/null; then
+    echo "Installing nix single user mode..."
+    curl -LO https://nixos.org/nix/install
+    chmod +x ./install
+    ./install --no-daemon --yes
+    . $HOME/.nix-profile/etc/profile.d/nix.sh
+    echo "--> done"
+    NIXVERSION=$(nix --version)
+    echo "$NIXVERSION is installed." >> ./setup.log
+else
+    NIXVERSION=$(nix --version)
+    echo "$NIXVERSION is installed." >> ./setup.log
+fi
+# https://devenv.sh/
+nix-env -iA devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
+devenv init
 
 # remove nix install script
 rm ./install
