fe6cd2210bdf6a39524b5be749994efa0b15dec1
torstenboettjer <torsten.boettjer@gmail.com>
2024-06-02 21:20:04 +0200

    feat: git config

--- nixdev.sh
+++ nixdev.sh
@@ -6,30 +6,21 @@ set -e
 # create log file
 touch ./setup.log
 
-#######################################
-# devenv.sh incl. nix
-#######################################
-install_devenv() {
-    if command -v nix &> /dev/null; then
-        NIXVERSION=$(nix --version)
-        echo "$NIXVERSION is installed." >> ./setup.log
-    else
-        echo "Installing nix single user mode..."
-        sh <(curl -L https://nixos.org/nix/install) --no-daemon --yes
-        . $HOME/.nix-profile/etc/profile.d/nix.sh
-        echo "--> done"
-        NIXVERSION=$(nix --version)
-        echo "$NIXVERSION is installed." >> ./setup.log
-    fi
-    nix-env -iA devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
-    devenv init
-}
+# https://nixos.org/
+if command -v nix &> /dev/null; then
+    NIXVERSION=$(nix --version)
+    echo "$NIXVERSION is installed." >> ./setup.log
+else
+    echo "Installing nix single user mode..."
+    sh <(curl -L https://nixos.org/nix/install) --no-daemon --yes
+    . $HOME/.nix-profile/etc/profile.d/nix.sh
+    echo "--> done"
+    NIXVERSION=$(nix --version)
+    echo "$NIXVERSION is installed." >> ./setup.log
+fi
+# https://devenv.sh/
+nix-env -iA devenv -f https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable
+devenv init
 
-# Select functions
-main() {
-    install_devenv
-    sudo ./ext.sh
-}
-
-# Execute main function with all the command-line arguments
-main "$@"
+# Install ChromeOS extensions
+sudo ./ext vsc
