7b6c03c4b0bc380b090d9b895b0c590908ac62b1
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2024-06-02 19:09:51 +0200

    Update nixdev.sh

--- nixdev.sh
+++ nixdev.sh
@@ -151,9 +151,7 @@ option_terraform() {
     gpg --no-default-keyring \
         --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
         --fingerprint
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-        https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-        tee /etc/apt/sources.list.d/hashicorp.list
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
     apt update && apt install terraform -y
     TF_VERSION=$(terraform version)
     echo "Installed Terraform $TF_VERSION" >> ./setup.log
